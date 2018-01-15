118291ae6b0073b56dd16580b41c5113c23c3e6f
tsujitoe <tsujitoe@gmail.com>
2018-01-15 21:54:24 +0800

    截圖不來啦!!

--- houserent/devhouse/admin.py
+++ houserent/devhouse/admin.py
@@ -8,7 +8,7 @@ from .models import Devinfo
 class Dvenifoadmin(admin.ModelAdmin):
 
 	list_display = ('state', 'dev_tracetime', 'dev_type', 'dev_zone','dev_name','dev_phone', 'image_tag',
-		'dev_address','dev_rent', 'dev_date', 'screen_tag', 'url_tag',)
+		'dev_address','dev_rent', 'dev_date', 'url_tag',)
 	fieldsets = (
 		['Main',{
 		'fields':('state', 'dev_tracetime','dev_name', 'dev_phone','dev_phone_img'),
