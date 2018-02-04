7e45d810a7098d4ac17aea51e1fb99f0e84068ec
tsujitoe <tsujitoe@gmail.com>
2018-02-04 11:45:48 +0800

    截圖改進+2

--- houserent/py_web2image.py
+++ houserent/py_web2image.py
@@ -60,34 +60,6 @@ with Display():
 
 
 
-
-
-
-infos = Devinfo.objects.all()
-for info in infos:
-	try:
-		im = Image.open(info.dev_screenshot_img)
-		#print (im.format, im.size, im.mode)
-		if im.size != 600:
-			width = 600
-			ratio = float(width)/im.size [0]
-			height = int(im.size [1] * ratio)
-
-			nim = im.resize((width, height), Image.BILINEAR)
-			nim.save('resize.png')
-			img_temp = open('resize.png', 'rb')
-
-			filename_screen = 'resize-'+info.dev_address
-
-			info.dev_screenshot_img.save('%s.png'%(filename_screen),File(img_temp), save=True)
-	
-			info.save()
-			print("縮圖ok-%s"%(info.dev_address))
-	except:
-		print("哪邊有問題?-%s"%(info.dev_address))
-
-
-
 """
 # pythonanywhere 沒辦法使用 phantomJS
 driver = webdriver.PhantomJS(executable_path='/Users/tsujitoe-mac/program/Python/houserent/houserent/static/node_modules/phantomjs/bin/phantomjs') # or add to your PATH
