5a551d7fcab07c070ac1c95622275876a40ea3c7
tsujitoe <tsujitoe@gmail.com>
2018-02-04 22:17:24 +0800

    改進replace

--- houserent/py_replace_pattern.py
+++ houserent/py_replace_pattern.py
@@ -1,39 +1,42 @@
 from devhouse.models import Devinfo
 
+pattern_word = {
+	"(室)":" ",  # 針對好房網
+	"3坪":"套房", # 以下針對591套房
+	"4坪":"套房",
+	"5坪":"套房",
+	"6坪":"套房",
+	"7坪":"套房",
+	"8坪":"套房",
+	"9坪":"套房",
+	"10坪":"套房",
+	"11坪":"套房",
+	"12坪":"套房",
+	"13坪":"套房",
+	"14坪":"套房",
+	"15坪":"套房",
+	"16坪":"套房",
+	"17坪":"套房",
+	"18坪":"套房",
+	"19坪":"套房",
+	"20坪":"套房",
+	"21坪":"套房",
+	"22坪":"套房",
+	"23坪":"套房",
+	"24坪":"套房",
+	"25坪":"套房",
+}
+
+
 infos = Devinfo.objects.all()
 
 for info in infos:
 	try:
 		pattern = info.dev_pattern
-		info.dev_pattern = pattern.replace("(室)","") # 針對好房網
-		info.dev_pattern = pattern.replace("3坪","套房") # 以下針對591套房
-		info.dev_pattern = pattern.replace("4坪","套房")
-		info.dev_pattern = pattern.replace("5坪","套房")
-		info.dev_pattern = pattern.replace("6坪","套房")
-		info.dev_pattern = pattern.replace("7坪","套房")
-		info.dev_pattern = pattern.replace("8坪","套房")
-		info.dev_pattern = pattern.replace("9坪","套房")
-		info.dev_pattern = pattern.replace("10坪","套房")
-		info.dev_pattern = pattern.replace("11坪","套房")
-		info.dev_pattern = pattern.replace("12坪","套房")
-		info.dev_pattern = pattern.replace("13坪","套房")
-		info.dev_pattern = pattern.replace("15坪","套房")
-		info.dev_pattern = pattern.replace("16坪","套房")
-		info.dev_pattern = pattern.replace("17坪","套房")
-		info.dev_pattern = pattern.replace("18坪","套房")
-		info.dev_pattern = pattern.replace("19坪","套房")
-		info.dev_pattern = pattern.replace("20坪","套房")
-		info.dev_pattern = pattern.replace("21坪","套房")
-		info.dev_pattern = pattern.replace("22坪","套房")
-		info.dev_pattern = pattern.replace("23坪","套房")
-		info.dev_pattern = pattern.replace("24坪","套房")
-		info.dev_pattern = pattern.replace("25坪","套房")
-		info.dev_pattern = pattern.replace("26坪","套房")
-		info.dev_pattern = pattern.replace("27坪","套房")
-		info.dev_pattern = pattern.replace("28坪","套房")
-		info.dev_pattern = pattern.replace("29坪","套房")
-
+		for word, change_word in pattern_word.items():
+			pattern = pattern.replace(word, change_word)
+			info.dev_pattern = pattern
 		info.save()
-		print('更改成功')
+		print('更改成功-%s' % info.dev_address)
 	except:
-		print('更改失敗')
+		print('更改失敗-%s' % info.dev_address)
